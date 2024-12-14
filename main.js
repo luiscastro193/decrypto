bc4af06a81b7a4707c9038d95ce310f56c61e354
luiscastro193 <luiscastro193@gmail.com>
2024-12-14 18:47:52 +0100

    Implement QR sharing

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const zipPromise = import("https://luiscastro193.github.io/zip-string/zip-string
 const endpoint = "https://luiscastro193.com/scraper/words";
 const codeElement = document.querySelector('p');
 const wordsElement = document.querySelector('ol');
-const [codeButton, wordsButton, shareButton] = document.querySelectorAll('button');
+const [codeButton, wordsButton, shareButton, qrButton] = document.querySelectorAll('button');
 
 async function request(resource, options) {
 	let response = await fetch(resource, options);
@@ -42,6 +42,7 @@ async function getWords() {
 			wordsElement.innerHTML = '';
 			wordsElement.append(...myWords.map(toItem));
 			shareButton.disabled = false;
+			qrButton.disabled = false;
 		});
 		
 		words.catch(error => {
@@ -89,13 +90,22 @@ wordsButton.onclick = async () => {
 	if (isValid) printCode();
 };
 
+async function shareURL() {
+	return new URL('#' + await (await zipPromise).zip((await getWords()).join('\n')), location.href);
+}
+
 shareButton.onclick = async () => {
-	let url = new URL('#' + await (await zipPromise).zip((await getWords()).join('\n')), location.href);
+	let url = await shareURL();
 	
 	if (navigator.share)
 		navigator.share({url});
 	else
 		navigator.clipboard.writeText(url).then(() => alert("Enlace copiado al portapapeles"));
+};
+
+qrButton.onclick = async () => {
+	let url = "https://luiscastro193.github.io/qr-generator/#" + await (await zipPromise).zip(await shareURL());
+	window.open(url);
 };
 
 window.onhashchange = () => location.reload();
